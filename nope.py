a505277d77fe5712e1c74d9b3bdc6c94a85ae6c1
pkorba <32198274+pkorba@users.noreply.github.com>
2025-06-04 16:55:12 +0200

    Add help, change redaction reason, remove reaction

--- nope.py
+++ nope.py
@@ -9,7 +9,10 @@ class NopeBot(Plugin):
         user_id = evt.sender
         bot_message_id = evt.content.get_reply_to()
         if bot_message_id is None:
-            # Message that user wants to delete is not direct reply to his previous command. Ignore silently
+            # Message that user wants to delete is not direct reply to his previous command
+            await evt.respond("**Usage:**  \n"
+                              "Reply to the bot's message that you want to redact with !nope  \n"
+                              "It only works with messages that are direct reply to your own command.")
             return
         bot_message: MessageEvent = await self.client.get_event(room_id=evt.room_id, event_id=bot_message_id)
         if bot_message.sender != self.client.mxid:
@@ -20,5 +23,4 @@ class NopeBot(Plugin):
             return
         user_message = await self.client.get_event(room_id=evt.room_id, event_id=user_message_id)
         if user_message.sender == user_id:
-            await self.client.redact(room_id=evt.room_id, event_id=bot_message_id, reason="Message redacted with !nope by command caller.")
-            await evt.react("👍")
+            await self.client.redact(room_id=evt.room_id, event_id=bot_message_id, reason=f"Message redacted with !nope by {user_id}")
