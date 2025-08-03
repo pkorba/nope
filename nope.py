239d9f1f1fc4ec8b13cafda48aaf1711bd938295
pkorba <32198274+pkorba@users.noreply.github.com>
2025-08-03 14:59:20 +0200

    Refactor code

--- nope.py
+++ nope.py
@@ -1,6 +1,6 @@
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
-from mautrix.types import EventType, ReactionEvent
+from mautrix.types import EventType, ReactionEvent, BaseRoomEvent
 from typing import Tuple
 
 
@@ -12,31 +12,16 @@ class NopeBot(Plugin):
         :param evt: message event
         """
         await evt.mark_read()
-        user_id = evt.sender
         bot_message_id = evt.content.get_reply_to()
         if bot_message_id is None:
             # Message that user wants to delete is not direct reply to his previous command
             await evt.respond("> **Usage:**  \n"
                               "> Reply to the bot's message that you want to remove with `!nope` or react to it with ❌ emoji.  \n"
                               "> It only works with messages that are direct reply to your own command "
-                              "or if you have permission to delete others messages in this room.")
+                              "or if you have permission to delete messages of other users in this room.")
             return
         bot_message: MessageEvent = await self.client.get_event(room_id=evt.room_id, event_id=bot_message_id)
-        if bot_message.sender != self.client.mxid:
-            return
-        user_message_id = bot_message.content.get_reply_to()
-        if user_message_id is None:
-            # Bot's response is not a reply so it wasn't initiated by the caller. Ignore silently
-            return
-        user_message = await self.client.get_event(room_id=evt.room_id, event_id=user_message_id)
-        try:
-            power_levels = await self.client.get_state_event(evt.room_id, EventType.ROOM_POWER_LEVELS)
-            user_level = power_levels.get_user_level(user_id)
-        except Exception as e:
-            self.log.error(f"Failed to check user power level: {e}")
-
-        if user_message.sender == user_id or user_level and user_level >= power_levels.redact:
-            await self.client.redact(room_id=evt.room_id, event_id=bot_message_id, reason=f"Message redacted with !nope by {user_id}")
+        await self.try_redact(evt, bot_message)
 
     @command.passive(regex="❌", field=lambda evt: evt.content.relates_to.key, event_type=EventType.REACTION, msgtypes=[])
     async def nope_react(self, evt: ReactionEvent, key: Tuple[str]) -> None:
@@ -44,9 +29,16 @@ class NopeBot(Plugin):
         Remove the bot's response to your own message by reacting with ❌ emoji to the bot's message.
         :param evt: message event
         """
-        user_id = evt.sender
         bot_message_id = evt.content.relates_to.event_id
-        bot_message  = await self.client.get_event(room_id=evt.room_id, event_id=bot_message_id)
+        bot_message = await self.client.get_event(room_id=evt.room_id, event_id=bot_message_id)
+        await self.try_redact(evt, bot_message)
+
+    async def try_redact(self, evt: BaseRoomEvent, bot_message: MessageEvent) -> None:
+        """
+        Remove message if user has sufficient permissions
+        :param evt: event initiating deletion process
+        :param bot_message: message to be deleted
+        """
         if bot_message.sender != self.client.mxid:
             return
         user_message_id = bot_message.content.get_reply_to()
@@ -54,11 +46,13 @@ class NopeBot(Plugin):
             # Bot's response is not a reply so it wasn't initiated by the caller. Ignore silently
             return
         user_message = await self.client.get_event(room_id=evt.room_id, event_id=user_message_id)
+        user_level = 0
+        power_levels = None
         try:
             power_levels = await self.client.get_state_event(evt.room_id, EventType.ROOM_POWER_LEVELS)
-            user_level = power_levels.get_user_level(user_id)
+            user_level = power_levels.get_user_level(evt.sender)
         except Exception as e:
             self.log.error(f"Failed to check user power level: {e}")
 
-        if user_message.sender == user_id or user_level and user_level >= power_levels.redact:
-            await self.client.redact(room_id=evt.room_id, event_id=bot_message_id, reason=f"Message redacted with !nope by {user_id}")
+        if user_message.sender == evt.sender or user_level and power_levels and user_level >= power_levels.redact:
+            await self.client.redact(room_id=evt.room_id, event_id=bot_message.event_id, reason=f"Message redacted with !nope by {evt.sender}")
