c44dc37089a30d29fc66e5b096ab4c03c56d5278
pkorba <32198274+pkorba@users.noreply.github.com>
2026-09-20 12:33:49 +0200

    Style changes

--- nope.py
+++ nope.py
@@ -1,7 +1,8 @@
+from typing import Tuple
+
+from mautrix.types import EventType, ReactionEvent, BaseRoomEvent
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
-from mautrix.types import EventType, ReactionEvent, BaseRoomEvent
-from typing import Tuple
 
 
 class NopeBot(Plugin):
@@ -15,15 +16,27 @@ class NopeBot(Plugin):
         bot_message_id = evt.content.get_reply_to()
         if bot_message_id is None:
             # Message that user wants to delete is not direct reply to his previous command
-            await evt.respond("> **Usage:**  \n"
-                              "> Reply to the bot's message that you want to remove with `!nope` or react to it with ❌ emoji.  \n"
-                              "> It only works with messages that are direct reply to your own command "
-                              "or reply to a command in general if you have permission to delete messages of other users in this room.")
+            await evt.respond(
+                "> **Usage:**  \n"
+                "> Reply to the bot's message that you want to remove with `!nope` or react to it"
+                " with ❌ emoji.  \n"
+                "> It only works with messages that are direct reply to your own command"
+                " or reply to a command in general if you have permission to delete messages of"
+                " other users in this room."
+            )
             return
-        bot_message: MessageEvent = await self.client.get_event(room_id=evt.room_id, event_id=bot_message_id)
+        bot_message: MessageEvent = await self.client.get_event(
+            room_id=evt.room_id,
+            event_id=bot_message_id
+        )
         await self.try_redact(evt, bot_message)
 
-    @command.passive(regex="❌", field=lambda evt: evt.content.relates_to.key, event_type=EventType.REACTION, msgtypes=[])
+    @command.passive(
+        regex="❌",
+        field=lambda evt: evt.content.relates_to.key,
+        event_type=EventType.REACTION,
+        msgtypes=[]
+    )
     async def nope_react(self, evt: ReactionEvent, key: Tuple[str]) -> None:
         """
         Remove the bot's response to your own message by reacting with ❌ emoji to the bot's message.
@@ -49,14 +62,29 @@ class NopeBot(Plugin):
         user_level = 0
         power_levels = None
         try:
-            power_levels = await self.client.get_state_event(evt.room_id, EventType.ROOM_POWER_LEVELS)
+            power_levels = await self.client.get_state_event(
+                evt.room_id,
+                EventType.ROOM_POWER_LEVELS
+            )
             # Create event contains information about the room owner in rooms v12
             # Their power level cannot be determined from PowerLevelStateEventContent alone
             state_events = await self.client.get_state(evt.room_id)
-            create_event = next((s_evt for s_evt in state_events if s_evt.type == EventType.ROOM_CREATE), None)
+            create_event = next(
+                (s_evt for s_evt in state_events if s_evt.type == EventType.ROOM_CREATE),
+                None
+            )
             user_level = power_levels.get_user_level(evt.sender, create_event)
         except Exception as e:
             self.log.error(f"Failed to check user power level: {e}")
 
-        if user_message.sender == evt.sender or user_level and power_levels and user_level >= power_levels.redact:
-            await self.client.redact(room_id=evt.room_id, event_id=bot_message.event_id, reason=f"Message redacted with !nope by {evt.sender}")
+        if (
+                user_message.sender == evt.sender
+                or user_level
+                and power_levels
+                and user_level >= power_levels.redact
+        ):
+            await self.client.redact(
+                room_id=evt.room_id,
+                event_id=bot_message.event_id,
+                reason=f"Message redacted with !nope by {evt.sender}"
+            )
